acd95f28a6ac29913923df1d8f389aa09fd0d3c9
Hudson Leone <leoneht0@gmail.com>
2025-02-10 22:31:28 -0700

    wrote allow_list. Need a nice way to config error code though... Can I default to 403?

--- mitmproxy/addons/blocklist.py
+++ mitmproxy/addons/blocklist.py
@@ -1,5 +1,6 @@
 from collections.abc import Sequence
 from typing import NamedTuple
+from typing import Optional
 
 from mitmproxy import ctx
 from mitmproxy import exceptions
@@ -35,10 +36,12 @@ def parse_spec(option: str) -> BlockSpec:
 
     return BlockSpec(matches=flow_filter, status_code=status_code)
 
+# TODO: write parse_spec for AllowSpec?
 
 class BlockList:
     def __init__(self) -> None:
-        self.items: list[BlockSpec] = []
+        self.blocked_items: Optional[list[BlockSpec]] = []
+        self.allowed_items: Optional[list[BlockSpec]] = []
 
     def load(self, loader):
         loader.add_option(
@@ -51,12 +54,35 @@ class BlockList:
             which requests this rule should be applied to and status-code is the HTTP status code to return for
             blocked requests. The separator ("/" in the example) can be any character.
             Setting a non-standard status code of 444 will close the connection without sending a response.
+            NOTE: This option cannot be used together with `allow_list` if both options are configured, an 
+            OptionsError will be raised. Configuring `blocked_items` will set `allowed_items` to None.
+            """,
+        )
+        loader.add_option(
+            "allow_list",
+            Sequence[str],
+            [],
+            """
+            Accept *only* the requests specified by the user. Any forbidden request is sent an empty response 
+            with a 403 status code. Option syntax is "/flow-filter/status-code", where flow-filter describes
+            which requests this rule should be applied to and status-code is the HTTP status code to return for
+            blocked requests. The separator ("/" in the example) can be any character.
+            Setting a non-standard status code of 444 will close the connection without sending a response.
+            NOTE: This option cannot be used together with `block_list` if both options are configured, an 
+            OptionsError will be raised. Configuring `allowed_items` will set `blocked_items` to None.
             """,
         )
 
     def configure(self, updated):
+
+        if "block_list" in updated and "allow_list" in updated:
+            raise exceptions.OptionsError(
+                "Cannot simultaneously configure options `block_list` and `allow_list`"
+            )
+
         if "block_list" in updated:
-            self.items = []
+            self.allowed_items = None
+            self.blocked_items = []
             for option in ctx.options.block_list:
                 try:
                     spec = parse_spec(option)
@@ -64,18 +90,47 @@ class BlockList:
                     raise exceptions.OptionsError(
                         f"Cannot parse block_list option {option}: {e}"
                     ) from e
-                self.items.append(spec)
+                self.blocked_items.append(spec)
+
+        if "allow_list" in updated:
+            self.allowed_items = []
+            self.blocked_items = None
+            for option in ctx.options.allow_list:
+                try:
+                    spec = parse_spec(option)
+                except ValueError as e:
+                    raise exceptions.OptionsError(
+                        f"Cannot parse block_list option {option}: {e}"
+                    ) from e
+                self.allowed_items.append(spec)
 
     def request(self, flow: http.HTTPFlow) -> None:
         if flow.response or flow.error or not flow.live:
             return
-
-        for spec in self.items:
-            if spec.matches(flow):
+        
+        if self.blocked_items != None:
+            for spec in self.blocked_items:
+                if spec.matches(flow):
+                    flow.metadata["blocklisted"] = True
+                    if spec.status_code == NO_RESPONSE:
+                        flow.kill()
+                    else:
+                        flow.response = http.Response.make(
+                            spec.status_code, headers={"Server": version.MITMPROXY}
+                        )
+        
+        if self.allowed_items != None:
+            global go_thru
+            go_thru = False
+            for spec in self.allowed_items:
+                if spec.matches(flow):
+                    go_thru = True
+                    break
+            if go_thru == False:
                 flow.metadata["blocklisted"] = True
                 if spec.status_code == NO_RESPONSE:
                     flow.kill()
                 else:
                     flow.response = http.Response.make(
                         spec.status_code, headers={"Server": version.MITMPROXY}
-                    )
+                        )
